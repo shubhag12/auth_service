92eac8a0a0567dff8e17e80a76c0433c355445ad
shubh agrwal <shubhagarwal123123@gmail.com>
2022-12-25 16:39:44 +0530

    password encryptiion added with bycrpt

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const bodyparser=require('body-parser');
 const { PORT } = require("./config/serverconfig");
-
+const {User}=require('./models/index');
 const apiroutes=require('./routes/index')
 const app = express();
+const bcrypt=require('bcrypt');
 const prepareAndStartServer = () => {
 
-  app.use(bodyparser.json);
+  app.use(bodyparser.json());
   app.use(bodyparser.urlencoded({extended:true}))
   app.use('/api',apiroutes);
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async() => {
     console.log(`server started ${PORT}`);
+    const incomingpassword='1234567';
+    const user=await User.findByPk(4); 
+    const response =bcrypt.compareSync(incomingpassword,user.password);
+    console.log(response);
   });
 };
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
+const bcrypt=require('bcrypt');
+const {salt, SALT}=require('../config/serverconfig')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -27,5 +29,11 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "User",
     }
   );
+  User.beforeCreate((user)=>{
+   // console.log(user);
+    const encryptedPassword=bcrypt.hashSync(user.password,SALT);
+     user.password=encryptedPassword
+
+  });
   return User;
 };
